cf8b70c1da55490ec380597c9d2671e6ec068f91
Cesar Genatios <cegenati@edu.uwaterloo.ca>
2020-03-29 16:55:53 -0600

    Fixed function to take in state variables (lm, lt and vm)

--- muscles.py
+++ muscles.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 class Hill_Type_Model:
 
-    def __init__(self, muscle, alpha):
+    def __init__(self, muscle, alpha, resting_length_muscle, resting_length_tendon):
         """
         parameters chosen based on participant 3 in supplemntary materials (source [2])
         :param alpha: FES activation (from our boi Trev)
@@ -18,6 +19,9 @@ class Hill_Type_Model:
         :param p_angle: penation angle
         """
         self.alpha = alpha
+        self.resting_length_muscle = resting_length_muscle
+        self.resting_length_tendon = resting_length_tendon
+
         if(muscle == "Tibialis Anterior"):
             # CONSTANTS FOR TibAnt
             self.Fmax = 523
@@ -49,21 +53,19 @@ class Hill_Type_Model:
             self.Vmax = 4
             self.p_angle = 17
 
-    def get_length_SE(self):
-        #TODO: figure out
-        return -1
-    
-    def get_length_CE(self):
-        # TODO: returns CE dependent time
-        return -1
+    def norm_tendon_length(self, muscle_tendon_length, normalized_muscle_length):
+        """
+        :param muscle_tendon_length: non-normalized length of the full muscle-tendon
+            complex (typically found from joint angles and musculoskeletal geometry)
+        :param normalized_muscle_length: normalized length of the contractile element
+            (the state variable of the muscle model)
+        :return: normalized length of the tendon
+        """
+        return (muscle_tendon_length - self.resting_length_muscle * normalized_muscle_length) / self.resting_length_tendon
 
-    def get_velocity_CE(self):
-        # TODO: returns velocity CE
-        return -1
-
-    def tendon_dynamics(self):
+    def tendon_dynamics(self, lt):
         # equations 7 & 8 in source [3]
-        lam = (self.get_length_SE() - self.lOpt)/self.lOpt
+        lam = (self.norm_tendon_length(lt, 1) - self.lOpt)/self.lOpt
         if lam <= 0:
             return 0
         else:
@@ -71,32 +73,53 @@ class Hill_Type_Model:
             denominator = np.exp(self.k_shape) - 1
             return self.Fmax * (numerator/denominator)
 
-    def get_force_length(self):
+    def get_force_length(self, lt):
         # source [2], equation 2
-        return (-1/self.w**2)*((self.get_length_CE()/self.lOpt)**2) + (2/(self.w**2))*(self.get_length_CE()/self.lOpt) - 1/(self.w**2) + 1
+        return (-1/self.w**2)*((self.norm_tendon_length(lt, 1)/self.lOpt)**2) + (2/(self.w**2))*(self.norm_tendon_length(lt,1)/self.lOpt) - 1/(self.w**2) + 1
 
-    def get_force_velocity(self):
+    def get_force_velocity(self, lm, lt):
         # source [2], equation 3
         N = 1.5
-        if self.get_velocity_CE() < 0:
-            return (self.Vmax + self.get_velocity_CE()) / (self.Vmax - self.k_curve*self.get_velocity_CE())
+        if self.get_velocity_CE(lm, lt) < 0:
+            return (self.Vmax + self.get_velocity_CE(lm, lt)) / (self.Vmax - self.k_curve*self.get_velocity_CE(lm, lt))
         else:
-            return N - ((N-1)*(self.Vmax + self.get_velocity_CE())) / (7.56*self.k_curve*self.get_velocity_CE() + self.Vmax)
+            return N - ((N-1)*(self.Vmax + self.get_velocity_CE(lm, lt))) / (7.56*self.k_curve*self.get_velocity_CE(lm, lt) + self.Vmax)
 
-    def get_force_contractile_element(self):
+    def get_force_contractile_element(self, lm, lt):
         # source: [2], equation 1
-        return self.alpha*self.Fmax*self.get_force_length()*self.get_force_velocity()
+        return self.alpha*self.Fmax*self.get_force_length(lt)*self.get_force_velocity(lm,lt)
 
-    def get_force_parallel_elastic(self):
+    def get_force_parallel_elastic(self,lm):
         # source [2], equation 4
-        if self.get_length_CE() <= (self.lOpt*(1-self.w)):
-            return self.Fmax*((self.get_length_CE()-self.lOpt)/(self.lOpt*self.w))**2
+        if self.norm_tendon_length(lm, 1) <= (self.lOpt*(1-self.w)):
+            return self.Fmax*((self.norm_tendon_length(lm, 1)-self.lOpt)/(self.lOpt*self.w))**2
         else:
             return 0
 
-    def get_force_series_elastic(self):
+    def get_force_series_elastic(self, lm, lt):
         # source: [3], equation adapted from simulation plan
-        return (self.get_force_contractile_element() + self.get_force_parallel_elastic())*np.cos(self.p_angle)
+        return (self.get_force_contractile_element(lm,lt) + self.get_force_parallel_elastic(lm))*np.cos(self.p_angle)
+
+    def get_velocity_CE(self, lm, lt):
+        """
+        :param a: activation (between 0 and 1)
+        :param lm: normalized length of muscle (contractile element)
+        :param lt: normalized length of tendon (series elastic element)
+        :return: normalized lengthening velocity of muscle (contractile element)
+        """
+        # WRITE CODE HERE TO CALCULATE VELOCITY
+        beta = 0.1 # damping coefficient (see damped model in Millard et al.)
+        sol = scipy.optimize.fsolve(self.model_dynamics, [0], (self.alpha,lm,lt,self.Fmax,beta))
+        #print (sol)
+        return sol
+
+    def model_dynamics(self, vm, lm, lt):
+        #TODO: Double check this model dynamics
+        return self.Fmax*(self.alpha*get_force_length(lt)*get_force_velocity(lm,lt)+get_force_parallel_elastic(lm)+0.1*vm-self.Fmax*self.get_force_length(lt))
+
+    def simulate(self):
+        #TODO: Add simulate function
+        return -1
 
 def plot_curves():
     """
